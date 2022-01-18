3af0689175a1791ea2257b3398d3abb915827def
ricHARD951 <75687540+ricHARD951@users.noreply.github.com>
2022-01-18 22:22:15 +0100

    Create Selecty.sql

---
+++ Selecty.sql
@@ -0,0 +1,35 @@
+#1.1 Select vypočíta priemernú odosielanú hodnotu a zapíše ju
+SELECT AVG(suma) AS"priemerna odosielna suma" FROM transakcie 
+
+#1.2 Select vypíše maximalnu odosielanú hodnotu
+SELECT MAX(suma) AS"maximalna odosielna suma" FROM transakcie
+
+#1.3 Select vypíše id uživatlela, dátum narodenia a zoradí ich vzostupne
+SELECT id, datum_narodenia FROM uzivatel ORDER	BY	datum_narodenia ASC
+
+#1.4
+SELECT 
+
+
+#2.1
+SELECT 
+#2.2
+SELECT
+#2.3
+SELECT
+
+
+#3.1
+SELECT
+#3.2
+SELECT
+#3.3
+SELECT
+
+
+#4.1
+SELECT
+#4.2
+SELECT
+
+
